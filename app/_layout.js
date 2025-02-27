69f598b74486f62e1acebfacef9745d57ef23001
businessashley3 <40072695-businessashley3@users.noreply.replit.com>
2025-02-27 20:11:23 +0000

    User checkpoint: Fix: Resolved "cannot read property 'id' of null" error on Breathe screen by removing unnecessary files and correcting tsconfig.json

--- app/_layout.js
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
-
-export default function RootLayout() {
-  return <AppNavigator />;
-}
--- app/index.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { Text, View } from 'react-native';
-
-export default function Index() {
-  // This file is needed for Expo but won't be displayed
-  // Our app will use the AppNavigator from _layout.js
-  return null;
-}
